d13cd053e7eaf322e79d3c669dcbf39556617375
nstitov_macbook <godlike200040@gmail.com>
2024-09-01 19:25:10 +0400

    update all ui for mac

--- orbisat/orbisat_gui/ui/ChooseGroundStationDialog.py
+++ orbisat/orbisat_gui/ui/ChooseGroundStationDialog.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'new_orbitron/orbitron_gui/ui/choose_ground_station_dialog.ui'
+# Form implementation generated from reading ui file 'choose_ground_station_dialog.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
@@ -24,6 +24,9 @@ class Ui_Dialog(object):
         Dialog.setMaximumSize(QtCore.QSize(320, 260))
         self.verticalLayoutWidget = QtWidgets.QWidget(Dialog)
         self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 0, 302, 259))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.verticalLayoutWidget.setFont(font)
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
         self.main_layout = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
         self.main_layout.setContentsMargins(0, 0, 0, 0)
@@ -38,9 +41,9 @@ class Ui_Dialog(object):
         self.choose_station_text_label.setMinimumSize(QtCore.QSize(300, 15))
         self.choose_station_text_label.setMaximumSize(QtCore.QSize(300, 15))
         font = QtGui.QFont()
-        font.setFamily("Open Sans")
-        font.setPointSize(10)
-        font.setItalic(True)
+        font.setFamily("Geneva")
+        font.setPointSize(12)
+        font.setItalic(False)
         self.choose_station_text_label.setFont(font)
         self.choose_station_text_label.setObjectName("choose_station_text_label")
         self.main_layout.addWidget(self.choose_station_text_label)
@@ -52,6 +55,9 @@ class Ui_Dialog(object):
         self.available_stations_scroll_area.setSizePolicy(sizePolicy)
         self.available_stations_scroll_area.setMinimumSize(QtCore.QSize(300, 200))
         self.available_stations_scroll_area.setMaximumSize(QtCore.QSize(300, 200))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.available_stations_scroll_area.setFont(font)
         self.available_stations_scroll_area.setWidgetResizable(True)
         self.available_stations_scroll_area.setObjectName("available_stations_scroll_area")
         self.scrollAreaWidgetContents = QtWidgets.QWidget()
@@ -69,6 +75,9 @@ class Ui_Dialog(object):
         self.choose_selected_station_button.setSizePolicy(sizePolicy)
         self.choose_selected_station_button.setMinimumSize(QtCore.QSize(145, 30))
         self.choose_selected_station_button.setMaximumSize(QtCore.QSize(145, 30))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.choose_selected_station_button.setFont(font)
         self.choose_selected_station_button.setObjectName("choose_selected_station_button")
         self.buttons_layout.addWidget(self.choose_selected_station_button)
         self.add_new_station_button = QtWidgets.QPushButton(self.verticalLayoutWidget)
@@ -79,6 +88,9 @@ class Ui_Dialog(object):
         self.add_new_station_button.setSizePolicy(sizePolicy)
         self.add_new_station_button.setMinimumSize(QtCore.QSize(145, 30))
         self.add_new_station_button.setMaximumSize(QtCore.QSize(145, 30))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.add_new_station_button.setFont(font)
         self.add_new_station_button.setObjectName("add_new_station_button")
         self.buttons_layout.addWidget(self.add_new_station_button)
         self.main_layout.addLayout(self.buttons_layout)
